278b344505817591a604fc21acdcaa8eb5de02d4
Yong <test@test.com>
2022-01-27 11:07:47 +0000

    Initial commit

---
+++ Source/WuKongEndlessRunner/WuKongEndlessRunner.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class WuKongEndlessRunner : ModuleRules
+{
+	public WuKongEndlessRunner(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
---
+++ Source/WuKongEndlessRunner/WuKongEndlessRunnerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WuKongEndlessRunnerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AWuKongEndlessRunnerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AWuKongEndlessRunnerGameMode();
+};
+
+
+
